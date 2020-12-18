e545cdb3dc83f972763c434e16a01ffc44041a27
Chris Paynter <chris@solanne.io>
2020-12-18 13:06:48 +0200

    Improved command ctrl laers

--- keyboards/sculpter/keymaps/chris/keymap.c
+++ keyboards/sculpter/keymaps/chris/keymap.c
@@ -21,9 +21,6 @@ enum layer_names {
     _QWERTY
 };
 
-enum custom_keycodes {
-    LEFT_COMMAND,
-};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
@@ -33,40 +30,55 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB, KC_SCOLON, KC_COMMA, KC_DOT, KC_P, KC_Y, KC_F, KC_G, KC_C, KC_R, KC_L, KC_SLASH, KC_AT, KC_BSLASH, KC_NO,
       KC_CAPS, KC_A, KC_O, KC_E, KC_U, KC_I, KC_D, KC_H, KC_T, KC_N, KC_S, KC_MINUS, KC_ENT, KC_NO, KC_NO,
       KC_LSFT, KC_QUOT, KC_Q, KC_J, KC_K, KC_X, KC_B, KC_M, KC_W, KC_V, KC_Z, KC_RSFT, KC_UP, KC_NO,
-      KC_LCTL, KC_LALT, LEFT_COMMAND, KC_SPC, KC_SPC, KC_RALT, KC_APP, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
+      KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, KC_SPC, KC_RALT, KC_APP, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
     [_QWERTY] = LAYOUT(
-      KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_PSCR, KC_SLCK, KC_PAUS, RESET,
+      KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_PSCR, KC_SLCK, KC_PAUS, KC_NO,
       KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_DEL, KC_HOME,
       KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS, KC_END,
       KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT, KC_INS, KC_PGUP,
       KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP, KC_PGDN,
-      KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_SPC, KC_RALT, KC_APP, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
+      KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, KC_SPC, KC_RALT, KC_APP, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     )
-}
+};
+
 void left_command_handler(keyrecord_t *record) {
-    static uint16_t timer;
+    // static uint16_t timer;
     if(record->event.pressed) {
-        timer = timer_read();
-        // register_mods(MOD_LGUI);
-        // layer_on(_QWERTY);
-        SEND_STRING("DOWN");
+        // timer = timer_read();
+        register_mods(MOD_LGUI);
+        layer_on(_QWERTY);
     } else {
-        // unregister_mods(MOD_LGUI);
-        // layer_off(_QWERTY);
-        SEND_STRING("UP");
-        if (timer_elapsed(timer) < TAPPING_TERM) {
+        unregister_mods(MOD_LGUI);
+        layer_off(_QWERTY);
+        // if (timer_elapsed(timer) < TAPPING_TERM) {
           // Can make the command button a tappable key also
-          // tap_code(KC_ESCAPE);
-          SEND_STRING("TAP");
-        }
+        // }
+    }
+}
+
+void left_control_handler(keyrecord_t *record) {
+    // static uint16_t timer;
+    if(record->event.pressed) {
+        // timer = timer_read();
+        register_mods(MOD_LCTL);
+        layer_on(_QWERTY);
+    } else {
+        unregister_mods(MOD_LCTL);
+        layer_off(_QWERTY);
+        // if (timer_elapsed(timer) < TAPPING_TERM) {
+          // Can make the command button a tappable key also
+        // }
     }
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-      case LEFT_COMMAND:
+      case KC_LGUI:
         left_command_handler(record);
+        return false;
+      case KC_LCTL:
+        left_control_handler(record);
         return false;
     }
     return true;
