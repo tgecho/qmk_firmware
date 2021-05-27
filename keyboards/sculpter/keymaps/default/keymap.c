0d4e97f3645b418a492c1f811369f598c4b23dd0
Erik Simmler <tgecho@gmail.com>
2021-05-27 11:13:29 -0400

    WIP

--- keyboards/sculpter/keymaps/default/keymap.c
+++ keyboards/sculpter/keymaps/default/keymap.c
@@ -14,6 +14,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include QMK_KEYBOARD_H
+// #include "print.h"
 
 // Defines names for use in layer keycodes and the keymap
 enum layer_names {
@@ -33,5 +34,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    // uprintf("KL: kc: 0x%04X, col: %u, row: %u, pressed: %b, time: %u, interrupt: %b, count: %u\n", keycode, record->event.key.col, record->event.key.row, record->event.pressed, record->event.time, record->tap.interrupted, record->tap.count);
     return true;
 }
